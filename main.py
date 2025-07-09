a213a864b500d94a61af4702427d8b1937b87367
Anar Algozha <Algozaanar@gmail.com>
2025-07-10 04:16:29 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI 
+from .database import Base, engine
+from .api import router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title = "Social Media App",
+    description = "Engine behind Social Media App",
+    version="0.1",
+
+)
+
+app.include_router(router)
